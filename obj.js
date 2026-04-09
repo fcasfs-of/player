ad4a6aeae7b09d1dd62ce72471495cfb59685f5b
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-09 06:52:18 -0300

    Criar obj.js

---
+++ obj.js
@@ -0,0 +1,60 @@
+function getUrlParameter(sParam) {  var dgetUrlParameterd="";
+    var sPageURL = decodeURIComponent(location.href);//window.location.search.substring(1));
+   if(sPageURL.split('?')){
+       var sURLVariables = sPageURL.split('?')[1].split('&');
+       if(sPageURL.split('?')[1].split('&')){
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if(sURLVariables[i].split('=')){
+        if (sParameterName[0] == sParam) {
+            dgetUrlParameterd=sParameterName[1];
+        }  }
+    }   }
+   }
+return dgetUrlParameterd;  }
+
+
+
+
+var getfval_tyget=getUrlParameter("fileID");
+	
+if (getfval_tyget!="") {
+	
+  var getfvaddl_tyget=getUrlParameter("cover");
+  var getfvaddl_listyget=getUrlParameter("list");
+
+var getfvadinf_listyget=getUrlParameter("info");
+
+
+var getfvald_tygetsle="1";
+var getfvald_tygetslpose="0";
+var getfvald_tyget=getUrlParameter("select");
+if (getfvald_tyget!="") {
+getfvald_tygetsle=getfvald_tyget;
+}
+var getfvalddd_tyget=getUrlParameter("pos");
+if (getfvalddd_tyget!="") {
+getfvald_tygetslpose=getfvalddd_tyget;
+}
+
+
+  var scriptfd = document.createElement("script");
+    scriptfd.setAttribute("type", "text/javascript");
+	scriptfd.setAttribute("id", "fileplayer_fd");
+    scriptfd.setAttribute("src", "https://player.fcasfs-of.cloud-fs.net/file/"+getfval_tyget+".js");
+document.getElementsByTagName("body")[0].appendChild(scriptfd);
+
+  function remover_id_playerfile(){  var infilefaz = document.getElementById("fileplayer_fd");     if(infilefaz){   infilefaz.remove();   }    		  }
+
+ 
+  var scrfiptfd = document.createElement("script");
+    scrfiptfd.setAttribute("type", "text/javascript");
+      scrfiptfd.setAttribute("onload", "onstart_file();");
+    scrfiptfd.setAttribute("src", "data:text/javascript,"+encodeURIComponent('    onstart_file(run_file);  remover_id_playerfile();   '));
+document.getElementsByTagName("body")[0].appendChild(scrfiptfd);
+  document.getElementsByTagName("body")[0].onload=function(){  onstart_file(run_file);   remover_id_playerfile();     };
+
+}   
+
+
+
