24dd3a713735497607ee8d530d17f9c8de75b8d6
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-12 19:10:26 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ onstart_fplay(fplayeri, getfvald_tygetslpose, getfvald_ddfffle,video.id);  }
             
             videoTitle.innerHTML = video.title;
 
-if(videoTitle.innerHTML=""){   fplayeri.OnEvents("init",function(){    videoTitle.innerHTML=""+fplayeri.api("title");  });   }
+if(videoTitle.innerHTML==""){   fplayeri.OnEvents("init",function(){    videoTitle.innerHTML=""+fplayeri.api("title");  });   }
 
 
 
