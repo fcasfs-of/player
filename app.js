1cf57589232c480822b5ff821695575ae5f96585
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-25 19:17:30 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -275,6 +275,16 @@ close: function(){  if(fplayeri){  fplayeri.api("play");  }  }
 
 }   }
 
+fcarregarTudo([
+    {
+        destino: 'head',  tag: 'script',   
+        atributos: {    
+			  'src': 'https://fcasfs-of.cloud-fs.net/sidebar/core.js'
+     }
+}
+]);
+
+
              
     //ampmpl.innerHTML='<iframe allowfullscreen width="100%" height="350" allow="Access-Control-Allow-Origin *; accelerometer *; ambient-light-sensor *; autoplay *; camera *; clipboard-read *; clipboard-write *; encrypted-media *; fullscreen *; geolocation *; gyroscope *; magnetometer *; microphone *; midi *; payment *; picture-in-picture *; screen-wake-lock *; speaker *; sync-xhr *; usb *; web-share *; vibrate *; vr *" sandbox="allow-downloads allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation allow-storage-access-by-user-activation" frameborder="0" scrolling="no" src="'+linkfromd+'" style="border: 1px solid black"></iframe>';  
     //amptar.innerHTML="<a href='"+linkfrarromd+"'>"+run_file().file_title+"</a>"; 
