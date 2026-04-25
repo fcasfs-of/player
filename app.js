c7c8a105942a906f1e591a5619f2d2dbfb249432
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-25 19:22:15 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -279,7 +279,8 @@ fcarregarTudo([
     {
         destino: 'head',  tag: 'script',   
         atributos: {    
-  'src': 'https://fcasfs-of.cloud-fs.net/sidebar/core.js'
+  'src': 'https://fcasfs-of.cloud-fs.net/sidebar/core.js',
+'onload': "initSidebar_start();"
      }
 }
 ]);
