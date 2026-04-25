73f3a2ef147d97eca7eac877ca1441a490c3dfba
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-25 17:18:05 -0300

    Atualizar o obj.js

--- obj.js
+++ obj.js
@@ -5,7 +5,7 @@ function fstringno_valtext(id,g) {   if (id == null || id === "" || id === "unde
 
 function F_getFileName(file) {
     const fileParam = new URLSearchParams(fstringno_valtext(file, ""));
-    return fileParam ? fileParam.replace(/\.[^/.]+$/, ' '') : null;
+    return fileParam ? fileParam.replace('/\.[^/.]+$/, ' ') : null;
 }
 
 
