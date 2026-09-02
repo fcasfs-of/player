432a750d3c9af3e76c8e3a63ef91c75f6e091720
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-02 15:46:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -402,7 +402,7 @@ stillist="";
     }  
 
     var getfval_tygddddet=getUrlParameter("full");
-	if (getfval_tygddddet && getfval_tygddddet=="on") {   sryrkddk=" <style>  .video-container { position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 2147483647; display: flex; } .video-container .iframe { width: 100%; height: 100%; flex: 1; border: none; margin: 0; padding: 0; }   </style>";  }
+	if (getfval_tygddddet && getfval_tygddddet=="on") {   sryrkddk=" <style>  .video-container { position: fixed !important; top: 0 !important; left: 0 !important; width: 100vw !important; height: 100vh !important; z-index: 2147483647 !important; display: flex !important; margin: 0 !important; padding: 0 !important; } .video-container .iframe, .video-container iframe { width: 100% !important; height: 100% !important; flex: 1 !important; border: none !important; margin: 0 !important; padding: 0 !important; display: block !important; }   </style>";  }
     
     acssr.innerHTML=sryrkk+sryrkddk;  
     document.title='Player: '+run_file().file_title+' - '+document.title;  
